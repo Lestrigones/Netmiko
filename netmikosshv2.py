02ec5fb49a3e33d0cda4023d58498445ef9acf79
Noelia <40467401+Lestrigones@users.noreply.github.com>
2019-11-05 13:06:59 +0100

    Add files via upload

---
+++ netmikosshv2.py
@@ -0,0 +1,19 @@
+from netmiko import ConnectHandler
+
+
+device_data = {
+            'device_type': 'cisco_ios',
+            'ip': '10.10.10.10',
+            'username': 'cisco',
+            'password': 'cisco'
+        }
+
+# net_connect=ConnectHandler(device_type= 'cisco_ios',ip='10.10.10.10',port='22',username='cisco',password='cisco')
+net_connect=ConnectHandler(**device_data)
+net_connect.find_prompt()
+output=net_connect.send_command("show ip int brief")
+output2=net_connect.send_command("show run")
+print(output)
+print(output2)
+
+
